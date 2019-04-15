e15c0791de40cacd161b8407362e8fa8afc2f1fd
Patt Kosit <pattabhum_patt@hotmail.com>
2019-04-15 14:08:07 +0700

    add basicInfoPageActivity.java crowdEstimationActivity.java editProfilePageActivity.java firstTimeLinkPaymentActivity.java homev2Activity.java passwordSignUpPage.java profilePageActivity.java add activity_basic_info_page.xml activity_crowd_estimation.xml activity_edit_profile_page.xml activity_first_time_link_payment.xml activity_homev2.xml activity_password_sign_up_page.xml activity_profile_page.xml confirm_profile_change_popup.xml order_row_layout.xml

---
+++ app/src/main/java/com/example/vcanteen/passwordSignUpPage.java
@@ -0,0 +1,13 @@
+package com.example.vcanteen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class passwordSignUpPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_sign_up_page);
+    }
+}
