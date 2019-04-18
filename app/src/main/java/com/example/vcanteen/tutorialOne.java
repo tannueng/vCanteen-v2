02b1a72254ed4bc9eaca4fdcd8516bd1e74641f9
Patt Kosit <pattabhum_patt@hotmail.com>
2019-04-18 13:01:57 +0700

    tutorial

---
+++ app/src/main/java/com/example/vcanteen/tutorialOne.java
@@ -0,0 +1,13 @@
+package com.example.vcanteen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tutorialOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial_one);
+    }
+}
