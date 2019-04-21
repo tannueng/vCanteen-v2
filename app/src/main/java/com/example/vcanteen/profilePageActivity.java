1caba7d0a751725c5120ae788e46143787e4945b
natnapinket <natnapinket@gmail.com>
2019-04-21 15:55:05 +0700

    profile page activity

--- app/src/main/java/com/example/vcanteen/profilePageActivity.java
+++ app/src/main/java/com/example/vcanteen/profilePageActivity.java
@@ -5,14 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.Glide;
 
 public class profilePageActivity extends AppCompatActivity {
 
     private Button editProfileButton;
+    private ImageView profilePictureImage;
+    private TextView name, email;
+    customerSingleton customerSingleton;
+    RequestOptions option = new RequestOptions().centerCrop();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
+        customerSingleton = com.example.vcanteen.customerSingleton.getInstance();
+
+        profilePictureImage = findViewById(R.id.profilePictureImage);
+        name = findViewById(R.id.Name);
+        email = findViewById(R.id.Email);
+
+        name.setText("" + customerSingleton.getFirstname() + " " + customerSingleton.getLastname());
+        email.setText(""+customerSingleton.getEmail());
+
+        Glide.with(profilePageActivity.this).load(customerSingleton.getCustomerImage()).apply(option).into(profilePictureImage);
 
         editProfileButton = findViewById(R.id.editProfileButton);
 
