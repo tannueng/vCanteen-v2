cd5838c90fc24576bca166ef688739c44d38c27c
New Akekamun <bingo3241@hotmail.com>
2019-04-26 14:55:58 +0700

    fixed notification 1st version

--- app/src/main/java/com/example/vcanteen/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/vcanteen/MyFirebaseMessagingService.java
@@ -1,10 +1,12 @@
 package com.example.vcanteen;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
@@ -12,13 +14,28 @@ import android.util.Log;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Map;
+
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    public static final String CHANNEL_ID = "Orders";
+    private static final String CHANNEL_NAME = "vCanteen Noti";
+    private static final String CHANNEL_DESC = "vCanteen Notification";
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
 
-        System.out.println("received message");
+        Log.i("testFCM", "startOnMessageReceived");
+
+        if (remoteMessage.getData() != null) {
+            sendNotification(remoteMessage);
+        }
+    }
+
+
+    private void sendNotification(RemoteMessage remoteMessage) {
+
         Intent intent = new Intent(this, OrderListActivity.class);
 
         PendingIntent pendingIntent = PendingIntent.getActivity(
@@ -28,46 +45,58 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 PendingIntent.FLAG_CANCEL_CURRENT
         );
 
-        if(remoteMessage.getData().size()>0) {
-            String title = remoteMessage.getNotification().getTitle();
-            String message = remoteMessage.getNotification().getBody();
-            Intent intent2 = new Intent("com.example.vcanteen_FCM-MESSAGE");
+        Map<String, String> data = remoteMessage.getData();
 
-            intent2.putExtra("title", title);
-            intent2.putExtra("message", message);
-            LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(this);
-            localBroadcastManager.sendBroadcast(intent2);
 
-            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "Orders")
-                    .setContentTitle(remoteMessage.getNotification().getTitle())
-                    .setContentText(remoteMessage.getNotification().getBody())
-                    .setContentIntent(pendingIntent)
-                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                    .setStyle(new NotificationCompat.BigTextStyle())
-                    .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-                    .setSmallIcon(R.mipmap.ic_launcher)
-                    .setAutoCancel(true);
+        String title = remoteMessage.getNotification().getTitle();
+        String body = remoteMessage.getNotification().getBody();
+//        String title = data.get("title");
+//        String body = data.get("body");
+        Log.i("testFCM", "if remotemessage.getdata() not null");
 
-            NotificationManager notificationManager =
-                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-            notificationManager.notify(1, notificationBuilder.build());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription(CHANNEL_DESC);
+//            NotificationManager manager = getSystemService(NotificationManager.class);
+//            manager.createNotificationChannel(channel);
+            notificationManager.createNotificationChannel(channel);
         }
 
-        if (remoteMessage.getNotification() != null) {
+//        NotificationHelper.displayNotification(getApplicationContext(), title, body);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+//                .setContentTitle(data.get("title"))
+//                .setContentText(data.get("body"))
+                .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setStyle(new NotificationCompat.BigTextStyle())
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(true);
 
 
+        notificationManager.notify(1, notificationBuilder.build());
 
 
-//            NotificationHelper.displayNotification(getApplicationContext(), title, body);
-
-        }
+//        if (remoteMessage.getData().size() > 0) {
+////            String title = remoteMessage.getNotification().getTitle();
+////            String body = remoteMessage.getNotification().getBody();
+//            Intent intent2 = new Intent("com.example.vcanteen_FCM-MESSAGE");
+//
+//            intent2.putExtra("title", title);
+//            intent2.putExtra("body", body);
+//            LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(this);
+//            localBroadcastManager.sendBroadcast(intent2);
+//
+//        }
     }
 
     @Override
     public void onNewToken(String mToken) {
         super.onNewToken(mToken);
-        Log.e("TOKEN",mToken);
+        Log.e("TOKEN", mToken);
     }
-
 }
